36b0562f74c7fd48a167e95f852dbff1a071cf5a
Ayush <ayushbansal095@gmail.com>
2022-05-05 17:18:15 +0530

    Updates Files

--- src/Utils/Firebase.js
+++ src/Utils/Firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZzqkjK_zqBv5sOnTDzq2gj5OHJF6sIp0",
-  authDomain: "veersa-af2d2.firebaseapp.com",
-  projectId: "veersa-af2d2",
-  storageBucket: "veersa-af2d2.appspot.com",
-  messagingSenderId: "654239989619",
-  appId: "1:654239989619:web:d5455e4a088441bc254f06",
-  measurementId: "G-SPT3BHVGXF"
+  apiKey: process.env.apiKey,
+  authDomain:  process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket:  process.env.storageBucket,
+  messagingSenderId:  process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId:  process.env.measurementId
 };
 
 const app = initializeApp(firebaseConfig);
